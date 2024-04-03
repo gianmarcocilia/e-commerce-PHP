4181d011341a588160a6d26d2c4b81a002a02b5a
gianmarcocilia <gianmarcocilia97@gmail.com>
2024-04-03 14:32:25 +0200

    refact: admin dashboard

--- admin/pages/dashboard.php
+++ admin/pages/dashboard.php
@@ -23,21 +23,11 @@
         <h2>Gli Ordini <i class="fa-solid fa-money-check"></i></h2>
         <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
         <a href="?page=orders-list" class="icon-link">
-          Call to action
+         Vai
           <svg class="bi" width="1em" height="1em"><use xlink:href="#chevron-right"></use></svg>
         </a>
       </div>
-      <div class="feature col">
-        <div class="feature-icon bg-primary bg-gradient">
-          <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg>
-        </div>
-        <h2>Gli Utenti <i class="fa-solid fa-user-group"></i></h2>
-        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-        <a href="#" class="icon-link">
-          Call to action
-          <svg class="bi" width="1em" height="1em"><use xlink:href="#chevron-right"></use></svg>
-        </a>
-      </div>
+      
       <div class="feature col">
         <div class="feature-icon bg-primary bg-gradient">
           <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg>
